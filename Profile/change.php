0b76ae69352cd376432fe69f2436bc217d117f2a
Poojashirshikar <165148557+Poojashirshikar@users.noreply.github.com>
2025-09-05 15:32:13 +0530

    Add files via upload

---
+++ Profile/change.php
@@ -0,0 +1,79 @@
+<!DOCTYPE HTML>
+
+<html lang="en">
+    <head>
+        <title>Profile: <?php echo $_SESSION['Username']; ?></title>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link href="bootstrap\css\bootstrap.min.css" rel="stylesheet">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="bootstrap\js\bootstrap.min.js"></script>
+        <link rel="stylesheet" href="assets/css/main.css" />
+		<meta charset="UTF-8">
+		<title>AgroCulture</title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<link href="bootstrap\css\bootstrap.min.css" rel="stylesheet">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="bootstrap\js\bootstrap.min.js"></script>
+		<!--[if lte IE 8]><script src="css/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="login.css"/>
+		<script src="js/jquery.min.js"></script>
+		<script src="js/skel.min.js"></script>
+		<script src="js/skel-layers.min.js"></script>
+		<script src="js/init.js"></script>     
+		<noscript>
+			<link rel="stylesheet" href="css/skel.css" />
+			<link rel="stylesheet" href="css/style.css" />
+			<link rel="stylesheet" href="css/style-xlarge.css" />
+		</noscript>
+		<link rel="stylesheet" href="indexfooter.css" />
+    </head>
+
+    <body class="subpage">
+
+        <?php
+            require 'menu.php';
+        ?>
+
+        <section id="post" class="wrapper bg-img" data-bg="banner2.jpg">
+            <div class="inner">
+                <div class="box">
+                <header>
+                    <span class="image left"><img src="<?php echo 'images/profileImages/'.$_SESSION['picName'].'?'.mt_rand(); ?>" class="img-circle" class="img-responsive" height="200px"></span>
+                    <br>
+                    <h2><?php echo $_SESSION['Name'];?></h2>
+                    <h4><?php echo $_SESSION['Username'];?></h4>
+                    <br>
+					<form id="password" method="post" action="changePass.php">
+					<div class="row uniform">
+									<div class="3u 12u$(xsmall)">
+			                            <input type="password" name="password" id="Hash" value="" placeholder="Old Password" required/>
+			                        </div>
+			                        <div class="3u 12u$(xsmall)">
+			                            <input type="password" name="pass" id="conNewPass" value="" placeholder=" New Password" required/>
+			                        </div>
+								</div>
+								 <div class="12u$">
+                        <center>
+                            <input type="submit" class="button special" value="Update Profile" />
+                        </center>
+                        </div>
+						</div>
+                </form>
+                </div>
+            </div>
+        </section>
+
+        <!-- Scripts -->
+            <script src="assets/js/jquery.min.js"></script>
+            <script src="assets/js/jquery.scrolly.min.js"></script>
+            <script src="assets/js/jquery.scrollex.min.js"></script>
+            <script src="assets/js/skel.min.js"></script>
+            <script src="assets/js/util.js"></script>
+            <script src="assets/js/main.js"></script>
+
+    </body>
+</html>
